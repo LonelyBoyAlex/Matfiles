cbf03bb43e5559467af7194552164e2a8d41d964
Alex <134187276+LonelyBoyAlex@users.noreply.github.com>
2026-01-28 12:32:02 +0530

    fixed niri background

---
+++ .config/niri/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent &
+~/.config/niri/scripts/swaync.sh &
+eww-daemon &
+swww-daemon &
+swww img $(readlink ~/.config/themes/active/wallpapers/current) &
+~/HyprlandScripts/ewwStarter.sh bar &
+swww-daemon -n wallpapes &
+swww img -n wallpapes .cache/wallblurred.png &
+~/.config/niri/scripts/swayidle.sh && notify-send 'swayidle' &
+udiskie --tray &
+nm-applet &
+blueman-applet &
--- .config/niri/scripts/overviewbackground.sh
+++ .config/niri/scripts/overviewbackground.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/env bash
-swaybg -i ~/.cache/wallblurred.png &
+#!/usr/bin/bash
+swww-daemon -n wallpapes &
+swww img -n wallpapes .cache/wallblurred.png
